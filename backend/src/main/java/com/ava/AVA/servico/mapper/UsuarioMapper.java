cd788405045e46d0e6b80a54ef2b3d884b6df85d
AlineCantalice <33903423+AlineCantalice@users.noreply.github.com>
2021-03-24 10:26:34 -0300

    Delete backend directory

--- backend/src/main/java/com/ava/AVA/servico/mapper/UsuarioMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ava.AVA.servico.mapper;
-
-import com.ava.AVA.domain.Usuario;
-import com.ava.AVA.servico.dto.UsuarioDTO;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface UsuarioMapper extends EntityMapper<UsuarioDTO, Usuario> {
-}
